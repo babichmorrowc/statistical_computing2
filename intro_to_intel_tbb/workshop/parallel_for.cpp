c2d1e050f148dd6d0e647b49712d9e04a2fb99a4
babichmorrowc <babichmorrowc@gmail.com>
2024-03-08 14:54:28 +0000

    Wrap up lecture notes

---
+++ intro_to_intel_tbb/workshop/parallel_for.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <tbb/parallel_for.h>
+
+int main(int argc, char **argv)
+{
+  auto values = std::vector<double>(10000);
+  
+  // for (int i=0; i<values.size(); ++i)
+  
+  tbb::parallel_for(
+    
+    // Entire range of values
+    tbb::blocked_range<int>(0, values.size()),
+    
+    // This is what each thread will do:
+    [&](tbb::blocked_range<int> r){
+      for (int i=r.begin(); i<r.end(); ++i)
+      {
+        values[i] = std::sin(i * 0.001);
+      }
+    }
+    
+  );
+  
+  
+  double total = 0;
+  
+  for (double value : values)
+  {
+    total += value;
+  }
+  
+  std::cout << total << std::endl;
+  
+  return 0;
+}
