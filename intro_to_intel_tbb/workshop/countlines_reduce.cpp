d17a87514bd45470940d011afbcf26725e0fcea2
babichmorrowc <babichmorrowc@gmail.com>
2024-04-15 07:04:45 +0100

    Work on Intel-TBB portfolio

--- intro_to_intel_tbb/workshop/countlines_reduce.cpp
+++ intro_to_intel_tbb/workshop/countlines_reduce.cpp
@@ -4,15 +4,18 @@
 using namespace part1;
 using namespace filecounter;
 
+int sum(int x, int y)
+{
+  return x + y;
+}
+
 int main(int argc, char **argv) // gives you access to command line arguments
 {
   auto filenames = get_arguments(argc, argv);
   auto results = map(count_lines, filenames);
+  auto total = reduce(sum, results);
   
-  for (int i = 0; i < filenames.size(); i++)
-  {
-    std::cout << filenames[i] << " has " << results[i] << " lines." << std::endl;
-  }
+  std::cout << "Total number of lines: " << total << std::endl;
   
   return 0;
   
